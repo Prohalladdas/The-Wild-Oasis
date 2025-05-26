1fe331df352b059347c80491c10b51d6068a4808
Prohalladdas <prolladdas@gmail.com>
2025-05-26 14:34:12 +0530

    Global Styles With Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 export default function App() {
-  return <div>hello world!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello world!</H1>
+        <Button onClick={() => alert("check in")}>Check in</Button>
+        <Button onClick={() => alert("check out")}>Check out</Button>
+        <Input type="text" placeholder="Enter your name" />
+        <Input type="text" placeholder="Enter your name" />
+      </StyledApp>
+    </>
+  );
 }
