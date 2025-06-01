aadc57e794886bedfe817aa3f7a8a7dd916a7928
Prohalladdas <prolladdas@gmail.com>
2025-06-01 19:39:58 +0530

    Mutations Deleting a Cabin

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -34,7 +34,7 @@ export default function CabinTable() {
     data: cabins,
     error,
   } = useQuery({
-    queryKey: ["canbin"],
+    queryKey: ["cabins"],
     queryFn: getCabins,
   });
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -14,3 +14,20 @@ export async function getCabins() {
     return data;
 
 }
+
+export async function deleteCanin(id) {
+
+    const { data, error } = await supabase
+        .from('cabins')
+        .delete()
+        .eq('id', id)
+
+
+    if (error) {
+        console.error(error);
+        throw new Error("Cabin could not be deleted");
+    }
+
+    return data;
+
+}
